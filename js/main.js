1c2109ad529990473fb5e6607fc57b93b9c13a0b
knlghtfreak <roshanahamedmba2022@gmail.com>
2025-04-13 11:53:50 +0530

    update: nav  scroll function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
 // action navbar
 
-
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
 // nav hide
 
 
