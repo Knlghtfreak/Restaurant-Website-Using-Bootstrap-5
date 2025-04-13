0a8b147ad1d4613da50ca0fe016d2ad7c4a2b3f5
knlghtfreak <roshanahamedmba2022@gmail.com>
2025-04-13 13:40:03 +0530

    update: counder ID

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
         current = start,
         range = end - start,
         increment = end > start ? 1 : -1,
-        step = math.abs(Math.floor(duration / range)),
+        step = Math.abs(Math.floor(duration / range)),
         time = setInterval(() => {
             current += increment;
             obj.textContent = current;
@@ -38,8 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }, step);
 
     }
-    counter("counter1", 0, 487, 3000);
-    counter("counter2", 0, 287, 3000);
-    counter("counter3", 0, 387, 3000);
-    counter("counter4", 0, 187, 3000);
+    counter("counter1", 0, 87, 1000);
+    counter("counter2", 0, 55, 1000);
+    counter("counter3", 0, 47, 1000);
+    counter("counter4", 0, 97, 1000);
 });
